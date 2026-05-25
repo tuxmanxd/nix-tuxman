8ef67de77c78d1b415d4aec5237b61101840f9e9
TuxMan <tuxkun09@gmail.com>
2026-05-25 15:59:33 +0100

    Add kitty Matugen and fastfetch config

--- home.nix
+++ home.nix
@@ -43,6 +43,10 @@
             name = "MesloLGS Nerd Font";
             size = 11;
         };
+        extraConfig = ''
+      include ~/.config/kitty/dank-theme.conf
+      include ~/.config/kitty/dank-tabs.conf
+    '';
     };
 
     home.file = {
@@ -50,6 +54,139 @@
       source = inputs.niri-dotfiles;
       recursive = true;
       force = true;
+    };
+  };
+
+  # Configure fastfetch
+  programs.fastfetch = {
+    enable = true;
+
+    settings = {
+      "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+
+      logo = {
+        padding = {
+          top = 1;
+        };
+      };
+
+      display = {
+        separator = " 󰑃  ";
+      };
+
+      modules = [
+        "break"
+        {
+          type = "os";
+          key = " DISTRO";
+          keyColor = "yellow";
+        }
+        {
+          type = "kernel";
+          key = "│ ├";
+          keyColor = "yellow";
+        }
+        {
+          type = "packages";
+          key = "│ ├󰏖";
+          keyColor = "yellow";
+        }
+        {
+          type = "command";
+          key = "│ ├";
+          keyColor = "yellow";
+          text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+        }
+        {
+          type = "shell";
+          key = "│ └";
+          keyColor = "yellow";
+        }
+        {
+          type = "wm";
+          key = " DE/WM";
+          keyColor = "blue";
+        }
+        {
+          type = "wmtheme";
+          key = "│ ├󰉼";
+          keyColor = "blue";
+        }
+        {
+          type = "icons";
+          key = "│ ├󰀻";
+          keyColor = "blue";
+        }
+        {
+          type = "cursor";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "terminalfont";
+          key = "│ ├";
+          keyColor = "blue";
+        }
+        {
+          type = "terminal";
+          key = "│ └";
+          keyColor = "blue";
+        }
+        {
+          type = "host";
+          key = "󰌢 SYSTEM";
+          keyColor = "green";
+        }
+        {
+          type = "cpu";
+          key = "│ ├󰻠";
+          keyColor = "green";
+        }
+        {
+          type = "gpu";
+          key = "│ ├󰻑";
+          format = "{2}";
+          keyColor = "green";
+        }
+        {
+          type = "display";
+          key = "│ ├󰍹";
+          keyColor = "green";
+          compactType = "original-with-refresh-rate";
+        }
+        {
+          type = "memory";
+          key = "│ ├󰾆";
+          keyColor = "green";
+        }
+        {
+          type = "swap";
+          key = "│ ├󰓡";
+          keyColor = "green";
+        }
+        {
+          type = "uptime";
+          key = "│ ├󰅐";
+          keyColor = "green";
+        }
+        {
+          type = "sound";
+          key = " AUDIO";
+          format = "{2}";
+          keyColor = "magenta";
+        }
+        {
+          type = "player";
+          key = "│ ├󰥠";
+          keyColor = "magenta";
+        }
+        {
+          type = "media";
+          key = "│ └󰝚";
+          keyColor = "magenta";
+        }
+        "break"
+      ];
     };
   };
 
