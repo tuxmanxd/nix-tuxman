b138878eb2eec2661e49e80a17cc35c541e4836b
TuxMan <tuxkun09@gmail.com>
2026-06-18 22:53:46 +0100

    Add OpenRGB for desktop

--- hosts/desktop/desktop-optimizations.nix
+++ hosts/desktop/desktop-optimizations.nix
@@ -6,6 +6,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  hardware.nvidia-container-toolkit.enable = true;
 
   # Enable Zram swap
   zramSwap = {
--- hosts/desktop/desktop-specifics.nix
+++ hosts/desktop/desktop-specifics.nix
@@ -11,6 +11,16 @@
     # Enable gamemode
     programs.gamemode.enable = true;
 
+    # Enable OpenRGB
+    services.hardware.openrgb = {
+      enable = true;
+      package = pkgs.openrgb-with-all-plugins;
+      server.port = 6742;
+    };
+
+    # Enable gpu passthrough to docker
+    virtualisation.docker.rootless.daemon.settings.features.cdi = true;
+
     environment.systemPackages = with pkgs; [
       heroic
       lutris
