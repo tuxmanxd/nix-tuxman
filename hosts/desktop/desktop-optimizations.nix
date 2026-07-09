c204a60697d50948653b90f468dcf176a6b115d6
TuxMan <tuxkun09@gmail.com>
2026-07-09 18:44:25 +0100

    Add all firmware aswell as force SOF driver for audio

--- hosts/desktop/desktop-optimizations.nix
+++ hosts/desktop/desktop-optimizations.nix
@@ -8,6 +8,9 @@
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
   hardware.nvidia-container-toolkit.enable = true;
 
+  boot.kernelParams = [ "snd_intel_dspcfg.dsp_driver=3" ];
+  hardware.enableAllFirmware = true;
+
   # Enable Zram swap
   zramSwap = {
     enable = true;
